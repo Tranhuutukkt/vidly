e7bbc0b1a55b5efd0677c8d5c14f728362c187ba
Trần Hữu An <tranhuutukkt@gmail.com>
2022-05-23 01:05:27 +0700

    edit movie route

--- routes/movies.js
+++ routes/movies.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const {Movie, validate} = require('../models/movie');
 const {Genre} = require('../models/genre')
 const auth = require("../middleware/auth");
@@ -33,7 +34,23 @@ router.put('/:id', async (req, res) => {
     const {error} = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const movie = await Movie.findByIdAndUpdate(req.params.id, {name: req.body.name}, {new: true});
+    const genre = await Genre.findById(req.body.genreId);
+    if (!genre) return res.status(400).send('Invalid genre!');
+
+    const movie = await Movie.findByIdAndUpdate(
+        req.params.id,
+        {
+            title: req.body.title,
+            genre: {
+                _id: genre._id,
+                name: genre.name
+            },
+            numberInStock: req.body.numberInStock,
+            dailyRentalRate: req.body.dailyRentalRate
+            },
+        {new: true}
+    );
+
     if (!movie) return res.status(404).send('Not found');
 
     res.send(movie);
@@ -48,6 +65,8 @@ router.delete('/:id', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id))
+        return res.status(404).send('Not found');
     const movie = await Movie.findById(req.params.id);
     if (!movie) return res.status(404).send('Not found');
 
